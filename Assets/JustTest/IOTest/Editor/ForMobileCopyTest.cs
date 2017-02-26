cf4f15e979b6e5de5514ceadd1fd7865d90516fc
afternooncg <afternooncg@126.com>
2017-02-27 01:02:12 +0800

    修复lua 打包测试 去除些无用测试文件persitdata/streamingasset

--- Assets/JustTest/IOTest/Editor/ForMobileCopyTest.cs
+++ Assets/JustTest/IOTest/Editor/ForMobileCopyTest.cs
@@ -18,36 +18,51 @@ public class ForMobileCopyTest
     static void BatSetAbNameOk()
     {
         string path = Application.dataPath + "/GameData/Lua";
-        string extensions = "*.bytes";
+        string extensions = "*.txt";
 
         Debug.Log(path);
 
-        /*
-        foreach (string file in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories))
+        FileListData data = AssetDatabase.LoadAssetAtPath<FileListData>("Assets/Resources/ScriptObjs/FileListData.asset");
+        for (int i = 0; i < data.Files.Count; i++)
         {
-            //UnityEditor.FileUtil.DeleteFileOrDirectory(file);
-            string tmp = Path.GetExtension(file).ToLower();
-            //Debug.Log(file + " " + tmp);
-            if (extensions.Contains(tmp))
-            {
-                Debug.Log("Assets/StreamingAssets/" + file.Replace(Application.streamingAssetsPath + "/", ""));
-                //AssetBundleHandle.SetAssetBundleName("Assets/StreamingAssets/" + file.Replace(Application.streamingAssetsPath + "/", ""));
-            }
+            AssetBundleHandle.SetAssetBundleName(data.Files[i], data.Files[i]+".unity3d");
+        }
 
-        }*/
+        AssetDatabase.Refresh();
+        AssetDatabase.SaveAssets();
+        /*
+
+            foreach (string file in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories))
+            {
+                //UnityEditor.FileUtil.DeleteFileOrDirectory(file);
+                string tmp = Path.GetExtension(file).ToLower();
+                //Debug.Log(file + " " + tmp);
+                if (extensions.Contains(tmp))
+                {
+                    Debug.Log("Assets/StreamingAssets/" + file.Replace(Application.streamingAssetsPath + "/", ""));
+                    AssetBundleHandle.SetAssetBundleName("Assets/StreamingAssets/" + file.Replace(Application.streamingAssetsPath + "/", ""));
+                }
+
+            }
+         */
     }
 
     [MenuItem("JustTest/ForMobileCopyTest/遍历StreamAssets_lua目录并生成文件列表写到FileListData.asset")]
     static void FillToFileListDataAsset()
     {
-        string path = Application.streamingAssetsPath + "/Lua";
+       // string path = Application.streamingAssetsPath + "/Lua";
 
+        string path = Application.dataPath + "/GameData/Lua";
+
+        //string extensions = "*.bytes";
         string extensions = "*.bytes";
 
         FileListData data = AssetDatabase.LoadAssetAtPath<FileListData>("Assets/Resources/ScriptObjs/FileListData.asset");
         data.Files.Clear();
 
         //Debug.Log(path);
+
+        string replaceStr = Application.dataPath.Replace("Assets", "");
 
         foreach (string file in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories))
         {
@@ -57,11 +72,15 @@ public class ForMobileCopyTest
             if (extensions.Contains(tmp))
             {
                 Debug.Log(file);
-                data.Files.Add(file.Replace(Application.streamingAssetsPath + "/", ""));
+                data.Files.Add(file.Replace(replaceStr, ""));
             }
 
         }
 
+        EditorUtility.SetDirty(data);
+        AssetDatabase.Refresh();
+        AssetDatabase.SaveAssets();
+        
     }
 
 
--- Assets/JustTest/IOTest/FileListData.cs
+++ Assets/JustTest/IOTest/FileListData.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class FileListData : ScriptableObject {
 
-
+    [SerializeField]
     public List<string> Files;
 
+    /*
     public FileListData()
     {
         Files = new List<string>();
     }
+     */
 }
