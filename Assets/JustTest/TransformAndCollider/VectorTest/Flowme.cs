89a4c24962f045f531b2a2d7519319876a888817
afternooncg <afternooncg@126.com>
2017-08-03 06:49:49 +0800

    vector 分解测试

---
+++ Assets/JustTest/TransformAndCollider/VectorTest/Flowme.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flowme : MonoBehaviour {
+
+    public   Transform hunter;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float vx = target.localPosition.x - hunter.localPosition.x;
+        float vy = target.localPosition.y - hunter.localPosition.y;
+
+
+        hunter.localPosition += new Vector3(vx, vy, 0f) * 0.001f;
+		
+	}
+}
